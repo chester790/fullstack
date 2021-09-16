3b4744336e982bb3ed0e98db70183d51b51c569f
Chester <2133807@talnet.nl>
2021-09-16 08:28:35 +0200

    made some changes to the website. Also added a new video to the site

--- home.php
+++ home.php
@@ -10,6 +10,7 @@ endif;
     <title>Main Menu</title>
     <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="header.css">
+    <link rel='icon' href="img/icon.jpg"/>
 </head>
 <body>
 
@@ -23,23 +24,37 @@ endif;
         <a href="home.php">Home</a>
         <a href="contact.html">Contact</a>
     </nav>
-
-    <h1>Welcome at Music For You!</h1>
-    <p>Press on a picture and watch the video!</p>
+    <div class="welcome">
+        <p>Press on a picture and watch the video!</p>
+    </div>
 
     <div class="video1">
-        <a href="https://music.youtube.com/watch?v=BFLNfXsfLhA&list=RDAMVMBFLNfXsfLhA"><img src="img/vid1.jpg" alt="video1" style="width:150px;height:100px;">
-
+        <a href="https://music.youtube.com/watch?v=BFLNfXsfLhA&list=RDAMVMBFLNfXsfLhA"><img src="img/vid1.jpg" alt="video1" style="width:200px;height:100px;"
+        </a>
     </div>
+    <div class="video2">
+        <a href="https://music.youtube.com/watch?v=y1xZ_kAhjMc&list=RDAMVMy1xZ_kAhjMc"><img src="img/vid2.jpg" alt="video2" style="width: 200px; height: 100px"
+        </a>
+    </div>
+    <div class="video3">
+        <a href="https://music.youtube.com/watch?v=UTHLKHL_whs&list=RDAMVMUTHLKHL_whs"><img src="img/vid3.jpg" alt="video3" style="width: 200px; height: 100px"
+        </a>
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
     <footer>
         <div class="links">
             <a href="home.php">Home</a>
             <a href="contact.html">Contact</a>
             <br>
-        </div>
-        <div id="copyright">
-            <p>  Copyright © Chester790</p>
         </div>
     </footer>
 
