1fe23182be821bb57d7bd589625ce16340aaa373
Chester <2133807@talnet.nl>
2021-09-20 11:38:52 +0200

    made an new layout

---
+++ feature.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Features</title>
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+
+</body>
+</html>
--- home.php
+++ home.php
@@ -19,44 +19,51 @@ endif;
     <header>
         <h2 style="text-align: center">Music For You!</h2>
     </header>
-
     <nav>
-        <a href="home.php">Home</a>
-        <a href="contact.html">Contact</a>
+        <ul class="menu">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="feature.php">Features</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
     </nav>
-    <div class="welcome">
-        <p>Press on a picture and watch the video!</p>
-    </div>
 
-    <div class="video1">
-        <a href="https://music.youtube.com/watch?v=BFLNfXsfLhA&list=RDAMVMBFLNfXsfLhA"><img src="img/vid1.jpg" alt="video1" style="width:200px;height:100px;"
-        </a>
-    </div>
-    <div class="video2">
-        <a href="https://music.youtube.com/watch?v=y1xZ_kAhjMc&list=RDAMVMy1xZ_kAhjMc"><img src="img/vid2.jpg" alt="video2" style="width: 200px; height: 100px"
-        </a>
-    </div>
-    <div class="video3">
-        <a href="https://music.youtube.com/watch?v=UTHLKHL_whs&list=RDAMVMUTHLKHL_whs"><img src="img/vid3.jpg" alt="video3" style="width: 200px; height: 100px"
-        </a>
-    </div>
-
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    <footer>
-        <div class="links">
-            <a href="home.php">Home</a>
-            <a href="contact.html">Contact</a>
-            <br>
+        <div class="welcome">
+            <p>Press on a picture and watch the video!</p>
         </div>
-    </footer>
 
-</body>
-</html>
+        <div class="video1">
+            <a href="https://music.youtube.com/watch?v=BFLNfXsfLhA&list=RDAMVMBFLNfXsfLhA">
+                <img src="img/vid1.jpg" alt="video1" style="width:200px;height:100px;">
+            </a>
+        </div>
+        <div class="video2">
+            <a href="https://music.youtube.com/watch?v=y1xZ_kAhjMc&list=RDAMVMy1xZ_kAhjMc">
+                <img src="img/vid2.jpg" alt="video2" style="width: 200px; height: 100px">
+            </a>
+        </div>
+        <div class="video3">
+            <a href="https://music.youtube.com/watch?v=UTHLKHL_whs&list=RDAMVMUTHLKHL_whs">
+                <img src="img/vid3.jpg" alt="video3" style="width: 200px; height: 100px">
+            </a>
+        </div>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <footer>
+            <div class="links">
+                <a href="home.php">Home</a>
+                <a href="contact.html">Contact</a>
+                <br>
+            </div>
+        </footer>
+
+    </body>
+    </html>
